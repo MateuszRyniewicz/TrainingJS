d4afb66056e8301225964557e8b5dc64d1f06be1
Mateusz <ryniuss@o2.pl>
2022-06-07 19:14:08 +0200

    billShow is done

--- BillSpritter/scripts.js
+++ BillSpritter/scripts.js
@@ -3,28 +3,26 @@ const people = document.querySelector('#people');
 const tip = document.querySelector('#tip');
 const button = document.querySelector('.count');
 const error = document.querySelector('.error');
-const costtInfo = document.querySelector('.cost-info');
+const costInfo = document.querySelector('.cost-info');
 const cost = document.querySelector('.cost');
 
-const showBall = () => {
-	if (price.value == '' || people.value == '' || tip.value == 0) {
-		error.textContent = 'The fields are empty';
-		costtInfo.style.display = 'none';
+const showBill = () => {
+	if (price.value == '' || people.value == '' || tip == 0) {
+		error.textContent = 'The filds are empty';
+		costInfo.style.display = 'none';
 	} else {
 		error.textContent = '';
 		countBill();
 	}
 };
 
-countBill = () => {
+const countBill = () => {
 	const newPrice = parseFloat(price.value);
 	const newPeople = parseInt(people.value);
 	const newTip = parseFloat(tip.value);
 
 	const suma = (newPrice + newPrice * newTip) / newPeople;
-	costtInfo.style.display = 'block';
-
-	cost.textContent = suma.toFixed(2);
+	costInfo.style.display = 'block';
+	costInfo.textContent = suma.toFixed(2);
 };
-
-button.addEventListener('click', showBall);
+button.addEventListener('click', showBill);
