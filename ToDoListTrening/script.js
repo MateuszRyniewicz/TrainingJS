be8ddd36affe55686fc8b881b9c9c39ce686ce1c
Mateusz <ryniuss@o2.pl>
2022-06-21 19:27:49 +0200

    projest is done

--- ToDoListTrening/script.js
+++ ToDoListTrening/script.js
@@ -33,6 +33,9 @@ const prepareDOMElements = () => {
 const prepareDOMEvents = () => {
 	$addBtn.addEventListener('click', addNewTask);
 	$todoInput.addEventListener('keyup', enterCheck);
+	$ulList.addEventListener('click', checkClick);
+	$addPopupBtn.addEventListener('click', changeToDo);
+	$closeTodoBtn.addEventListener('click', closePopup);
 };
 
 const addNewTask = () => {
@@ -45,6 +48,7 @@ const addNewTask = () => {
 
 		$todoInput.value = '';
 		$alertInfo.innerText = '';
+		createToolsArea();
 	} else {
 		$alertInfo.innerText = 'You have to write your task!';
 	}
@@ -54,6 +58,74 @@ const enterCheck = () => {
 	if (event.code === 'Enter') {
 		addNewTask();
 	}
+};
+
+const createToolsArea = () => {
+	toolsPanel = document.createElement('div');
+	toolsPanel.classList.add('tools');
+	$newTask.appendChild(toolsPanel);
+
+	const completeBtn = document.createElement('button');
+	completeBtn.classList.add('complete');
+	completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+
+	const editBtn = document.createElement('button');
+	editBtn.classList.add('edit');
+	editBtn.innerHTML = 'Edit';
+
+	const deleteBtn = document.createElement('button');
+	deleteBtn.classList.add('delete');
+	deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+
+	toolsPanel.appendChild(completeBtn);
+	toolsPanel.appendChild(editBtn);
+	toolsPanel.appendChild(deleteBtn);
+};
+
+const checkClick = (e) => {
+	if (e.target.classList.value !== '') {
+		if (e.target.closest('button').classList.contains('complete')) {
+			e.target.closest('li').classList.toggle('completed');
+			e.target.closest('button').classList.toggle('completed');
+		} else if (e.target.closest('button').classList.contains('edit')) {
+			editTask(e);
+		} else if (e.target.closest('button').classList.contains('delete')) {
+			deleteTask(e);
+		}
+	}
+};
+
+const editTask = (e) => {
+	const oldTodo = e.target.closest('li').id;
+	console.log(oldTodo);
+	$editedTodo = document.getElementById(oldTodo);
+
+	$popupInput.value = $editedTodo.firstChild.textContent;
+	$popup.style.display = 'flex';
+};
+
+const deleteTask = (e) => {
+	const deleteToTask = e.target.closest('li');
+	deleteToTask.remove();
+
+	if ($allTasks.length === 0) {
+		$alertInfo.innerHTML = 'The list is empty';
+	}
+};
+
+const changeToDo = () => {
+	if ($popupInput.value !== '') {
+		$editedTodo.firstChild.textContent = $popupInput.value;
+		$popupInput.style.display = 'none';
+		$popupInfo.innerText = '';
+	} else {
+		$popupInfo.innerText = 'You need to write some text';
+	}
+};
+
+const closePopup = () => {
+	$popup.style.display = 'none';
+	$popupInfo.innerText = '';
 };
 
 document.addEventListener('DOMContentLoaded', main);
